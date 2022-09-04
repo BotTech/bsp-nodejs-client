af2d63acea21eeb35b9701981a34a8b754c0d812
Jason Pickens <jasonpickensnz@gmail.com>
2022-09-05 10:59:21 +1200

    Add Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const A = 123
+export const A = 123;
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,111 +6,112 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'bsp-nodejs-client',
-  tagline: 'BSP client library for Node.js',
-  url: 'https://BotTech.github.io',
-  baseUrl: '/bsp-nodejs-client/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+	title: 'bsp-nodejs-client',
+	tagline: 'BSP client library for Node.js',
+	url: 'https://BotTech.github.io',
+	baseUrl: '/bsp-nodejs-client/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'throw',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'BotTech', // Usually your GitHub org/user name.
-  projectName: 'bsp-nodejs-client', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'BotTech', // Usually your GitHub org/user name.
+	projectName: 'bsp-nodejs-client', // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath: '/', // Serve the docs at the site's root
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/BotTech/bsp-nodejs-client/tree/main/website/',
-        },
-        blog: false,
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					routeBasePath: '/', // Serve the docs at the site's root
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						'https://github.com/BotTech/bsp-nodejs-client/tree/main/website/',
+				},
+				blog: false,
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'bsp-nodejs-client',
-        // logo: {
-        //   alt: 'My Site Logo',
-        //   src: 'img/logo.svg',
-        // },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
-          },
-          {
-            href: 'https://github.com/BotTech/bsp-nodejs-client',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Get Started',
-                to: '/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Issues',
-                href: 'https://github.com/BotTech/bsp-nodejs-client/issues',
-              },
-              {
-                label: 'Discussions',
-                href: 'https://github.com/BotTech/bsp-nodejs-client/discussions',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/BotTech/bsp-nodejs-client',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} BotTech, Ltd. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: 'bsp-nodejs-client',
+				// logo: {
+				//   alt: 'My Site Logo',
+				//   src: 'img/logo.svg',
+				// },
+				items: [
+					{
+						type: 'doc',
+						docId: 'intro',
+						position: 'left',
+						label: 'Docs',
+					},
+					{
+						href: 'https://github.com/BotTech/bsp-nodejs-client',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{
+								label: 'Get Started',
+								to: '/intro',
+							},
+						],
+					},
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Issues',
+								href: 'https://github.com/BotTech/bsp-nodejs-client/issues',
+							},
+							{
+								label: 'Discussions',
+								href: 'https://github.com/BotTech/bsp-nodejs-client/discussions',
+							},
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'GitHub',
+								href: 'https://github.com/BotTech/bsp-nodejs-client',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} BotTech, Ltd. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
 };
 
 module.exports = config;
